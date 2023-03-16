4ad5b26f6ef8656a9ed4c9ba8d662928bf1e17cf
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-16 16:33:57 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - our fucntion/entry point
- *
- * Description: a C program that prints with puts function
- *
- * Return: Always 0 integer
- * Return: Always 0 integer
-*/
-
 int main(void)
 {
 	puts "\"Programming is like building a multilingual puzzle";
