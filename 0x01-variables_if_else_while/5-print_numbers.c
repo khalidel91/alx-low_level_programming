7e08a81eece93a8f633efb18ec1afea226ff02bd
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-17 16:15:16 -0700

    prints digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - our fucntion/entry point
+*
+* Description: 'a C program that prints digits from 0 to 9'
+*
+* Return: Always 0 integer
+*/
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i <= 9; ++i)
+	printf ("%d", i);
+	putchar('\n');
+	return (0);
+}
