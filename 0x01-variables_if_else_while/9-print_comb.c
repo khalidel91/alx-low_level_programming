181259f18af7ebee5dcdcf399a4cf24dce28d2ad
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-17 16:41:14 -0700

    prints digits using putchar

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - our fucntion/entry point
+*
+* Description: 'a C program that prints digits from 0 to 9 separated by commas'
+*
+* Return: Always 0 integer
+*/
+int main(void)
+
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ++ch)
+	{
+		putchar(ch);
+		if (ch != '9')
+			putchar(', ');
+	}
+	putchar('\n');
+	return (0);
+}
