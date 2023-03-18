a4fc277fb035d627e83a61a9548fa3f59efcc5e9
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-17 17:45:39 -0700

    print all possible combination of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,10 +21,11 @@ int main(void)
 			putchar(i);
 			putchar(j);
 			putchar(k);
-			if ((i == '7') && (j == '9') && (k == '9'))
+			if ((i == '7') && (j == '8') && (k == '9'))
 				break;
 			putchar(',');
-			putchar(' ');
+			putchar(' ');i
+			}
 		}
 	}
 	putchar('\n');
