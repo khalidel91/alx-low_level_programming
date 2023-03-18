b0f2663bae5347ed1fd029a89db3da925247b9d8
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-17 17:08:10 -0700

    print all cobination from 01 to 99

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - our fucntion/entry point
+*
+* Description: 'a C program that prints digits from 0 to 9 separated by commas'
+*
+* Return: Always 0 integer
+*/
+int main(void)
+
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; ++i)
+	{
+		putchar(i);
+		for (j = 'i+1'; j <= '9'; j++)
+		{
+			putchar('j');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
