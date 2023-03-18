39ade62f669cf97ebd9ae9790a310398d65d8abe
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-17 18:04:09 -0700

    tha last one

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - our fucntion/entry point
+*
+* Description: 'a C program that prints digits from 0 to 9 separated by commas'
+*
+* Return: Always 0 integer
+*/
+int main(void)
+
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; ++i)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = j; k <= '9'; k++)
+			{
+				for (l = k + 1; l <= 9; l++)
+				{
+				putchar(i);
+				putchar(j);
+				putchar(' ');
+				putchar(k);
+				putchar(l);
+				putchar(',');
+				putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
