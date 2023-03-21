186bccb6a8937ad9c6bf29447cf894bf9a971950
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-21 15:24:00 -0700

    print alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,18 @@
-first task  that print _putchar
+#include <stdio.h>
+/**
+* main - our fucntion/entry point
+*
+* Description: 'a C program that prints alphabet'
+*
+* Return: Always 0 integer
+*/
+int main(void)
+
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
