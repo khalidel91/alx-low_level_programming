a7c74bd7cdce147c808bf0e6c7d2a9130f9558a2
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-21 16:40:45 -0700

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
 * main - our fucntion/entry point
 *
@@ -12,7 +12,7 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ++ch)
-	putchar(ch);
-	putchar('\n');
+	_putchar(ch);
+	_putchar('\n');
 	return (0);
 }
