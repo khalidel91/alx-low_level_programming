f6ae8d738e6a6ba1526f63fcd9912e68ea34edae
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-21 19:35:10 -0700

    add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add  - prints the table nine time, starting with 0
+ *
+ * @a: first number
+ * @b: second number
+ * Return: void
+*/
+int add(int a, int b)
+{
+	int r;
+	r= a + b;
+	return (r);
+	
+	}
