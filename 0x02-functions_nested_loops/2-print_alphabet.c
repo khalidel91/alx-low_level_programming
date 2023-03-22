c108a0d48c790e85fe7eddf1094d497a97a7eead
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-21 18:40:06 -0700

    restore removed file

---
+++ 0x02-functions_nested_loops/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet - our fucntion/entry point
+*
+* Description: 'a C program that prints alphabet'
+*
+* Return: Always 0 integer
+*/
+void print_alphabet(void)
+
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+	_putchar(ch);
+	_putchar('\n');
+	return (0);
+}
