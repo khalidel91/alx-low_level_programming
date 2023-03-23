cd155be9426bf91e8e563a887fd93e863821440c
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-22 18:24:12 -0700

    100 times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,7 +24,7 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar(k + '0');
 			}
-			else
+			else if (k < 100)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -32,7 +32,15 @@ void print_times_table(int n)
 				_putchar(k / 10 + '0');
 				_putchar(k % 10 + '0');
 			}
-		}
+			else
+			{
+				_putchar(',');
+                                _putchar(' ');
+                                _putchar(' ');
+                                _putchar(k / 100 + '0');
+                                _putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+		}i
 		_putchar('\n');
 	}
 }
