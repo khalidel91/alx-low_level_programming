40a842d8d357965e27c789c05a7036df025d1eaf
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-22 18:51:38 -0700

    multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entrypoint
+ *
+ * 
+ *
+ * Description: function check if a number is greater or less or equal to 0
+ *
+ * Return: 1 mean greater & -1 mean less
+ */
+
+int main(void)
+{
+	int sum, 
+
+	for (i = 0; i < 1024; i++)
+	if (i % 3 == 0)
+		sum = sum + i;
+	else if (i % 5 == 0)
+		sum = sim + i;
+	printf("%d", sum);
+
+}
