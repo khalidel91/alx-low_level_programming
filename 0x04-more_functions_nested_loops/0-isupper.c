41e4ad5e29857f16101376612537828736a0cd9b
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-23 15:13:00 -0700

    first task more functions and loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _isupper - our fucntion/entry point
+* @c: character to be checked
+*
+* Description: 'a C program that check if a charachter is uppercase'
+*
+* Return: 1 if it's upper and 0 if not
+*/
+int _isupper(int c)
+	{
+		if (c > 64 && c < 91)
+		return (1);
+		else
+		return (0);
+	}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Hi, this diroctory is for tasks related to 0x04-more functions nested loops
