49d9e48635ca37ae4130e58cb0a0497d7d2a6db4
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-22 18:52:50 -0700

    multiples of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,13 +11,13 @@
 
 int main(void)
 {
-	int sum, 
+	int sum, i;
 
 	for (i = 0; i < 1024; i++)
 	if (i % 3 == 0)
 		sum = sum + i;
 	else if (i % 5 == 0)
-		sum = sim + i;
+		sum = sum + i;
 	printf("%d", sum);
 
 }
