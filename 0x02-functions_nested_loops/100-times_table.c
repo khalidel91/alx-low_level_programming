e858346037b9c149e9a74d5c1118ec140ab27d3e
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-22 18:41:20 -0700

    100 times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * times_table - prints the table nine time, starting with 0
- *
+ * print_times_table - prints the table nine time, starting with 0
+ * @n : dimension of table
  * Return: void
 */
 void print_times_table(int n)
@@ -36,9 +36,9 @@ if (n <= 15 && n >= 0)
 			else
 			{
 				_putchar(',');
-                                _putchar(' ');
-                                _putchar(k / 100 + '0');
-                                _putchar((k % 100) / 10 + '0');
+				_putchar(' ');
+				_putchar(k / 100 + '0');
+				_putchar((k % 100) / 10 + '0');
 				_putchar(k % 10 + '0');
 			}
 		}
