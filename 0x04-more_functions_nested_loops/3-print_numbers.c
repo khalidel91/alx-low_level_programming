5551ec64180ecc2487111e7acbc6c1575f659b17
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-23 15:42:51 -0700

    function that print cumbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ *
+ * description: function that print numbers from 0 to 9
+ *
+ * Return: Always 0
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('i');
+	}
+	_putchar('\n');
+}
