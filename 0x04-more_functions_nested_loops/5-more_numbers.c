1a76f999477d5a7fc2e21630a4feee4f15bf307a
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-23 16:50:25 -0700

    print more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers
+ *
+ * description: function that print more numbers
+ *
+ *
+ *
+ */
+
+void more_numbers(void)
+{
+	int i = 0, j;
+	for (j = 0; j < 10; j++)
+	{
+
+		for (i = 0; i < 15; i++)
+		{
+		if (i < 10) 
+		_putchar('0' + i);
+		else
+			_putchar('1');
+			_putchar('0' + i % 10)
+
+		}
+	_putchar('\n');
+	}
+}
