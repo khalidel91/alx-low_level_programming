19bea6f583e00ebeedb8a66dfbe5ff8aeea9c867
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-23 16:26:36 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers
+ *
+ * description: function that print most numbers
+ *
+ *
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		else
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
