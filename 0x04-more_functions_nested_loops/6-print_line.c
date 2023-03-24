f20880973f526cfe278700644f1369a1709a2fe2
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-23 17:49:06 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 /**
  * print_line - print numbers
  *
- *
+ * @n: parameter number of underscore
  * return: it's a void
  *
  */
@@ -17,7 +17,7 @@ void print_line(int n)
 	}
 	else
 	{
-	for (i = 0; i < n; i++)
+	for (i = 0;i < n; i++)
 	{
 			_putchar('_');
 	}
