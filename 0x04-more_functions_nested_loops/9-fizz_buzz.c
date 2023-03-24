88b29a0ce4c0e73737738011af49bd5af02b2acb
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-23 18:57:48 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print fizzfuzz
+ *
+ *
+ * return: it's a void
+ *
+ */
+void main(void)
+{
+	int i = 0;
+
+		for (i = 1; i <= 100; i++)
+		{
+			if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+			if ((i % 3 != 0) && (i % 5 != 0))
+			{
+				printf("%d", i);
+			}
+			printf(" ");
+		}
+}
