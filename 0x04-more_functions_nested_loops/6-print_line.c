4fa0302c7ad01be69283f6ab2a9eac9b569f0ef1
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-23 17:41:37 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,17 +9,18 @@
 
 void print_line(int n)
 {
+	int i = 0;
+
 	if (n =< 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 			_putchar('_');
 	}
-	}
 	_putchar('\n');
+	}
 }
