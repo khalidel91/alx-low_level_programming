b83806d0b99eedcacbbc389fbed9686f0935fb1d
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-23 18:17:10 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal - print numbers
+ *
+ * @n: parameter number of underscore
+ * return: it's a void
+ *
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (j = 0; j < n; j++)
+		{
+		for (i = 0; i < j; i++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		}
+	_putchar('\n');
+	}
+}
