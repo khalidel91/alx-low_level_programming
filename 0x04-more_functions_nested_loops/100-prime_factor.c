aa351034aeb91dae96e887e194329175f559d8f0
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-24 18:47:39 -0700

    largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,37 +6,44 @@
  * return: always 0
  *
  */
+#include <stdio.h>
+/**
+ * main - print square
+ *
+ * Description: largest factor
+ * return: always 0
+ *
+ */
 int main(void)
 {
-	int i = 0
+        long int number = 612852475;
+        int is_Prime(long int p)
+        {
+        if (p <= 1)
+        {
+        return (0);
+        }
+        for (long int i = 2; i <= p / 2; i++)
+        {
+                if (p % i == 0)
+                {
+                return (0);
+                }
+        }
+        return (1);
+        }
 
-	bool is_Prime(int p)
-	{
-	if (p <= 1)
-	{
-	return (false);
-	}
-	for (int i = 2; i <= p / 2; i++)
-	{
-		if (p % i == 0)
-		{
-		return (false);
-		}
-	}
-	return (true);
-	}
-	int number = 612852475143;
 
-	for (int i = number / 2; i >= 2; i--)
-	{
-	if (number % i == 0)
-	{
-	if (is_Prime(i))
-	{
-		printf("%d\n", i);
-		break;
-	}
-	}
-	}
-	return (0);
+        for (long int i = number / 2; i >= 2; i--)
+        {
+ if (number % i == 0)
+        {
+        if (is_Prime(i))
+        {
+                printf("%ld\n", i);
+                break;
+        }
+        }
+        }
+        return (0);
 }
