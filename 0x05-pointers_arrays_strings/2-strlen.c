37143cb848a7215615cf58e9e542217c04d431ae
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-27 18:38:48 -0700

    3rd task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - function that calculate the lenth of a string
+ *
+ * description: reset to n
+ *
+ * @s: string
+ * Return: i the lengh of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i + 1)
+}
