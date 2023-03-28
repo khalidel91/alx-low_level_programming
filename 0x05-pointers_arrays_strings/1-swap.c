b52b341c40fb18c981a1cfece7230fcb6a57400e
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-27 18:25:41 -0700

    second task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * swap_int - print numbers
+ *
+ * description: reset to n
+ *
+ * @a: pointer
+ * @b: pointer b
+ */
+
+void swap_int(int *a, int *b);
+{
+	int t;
+
+	t = *a;
+	*a = *b;
+	*b = t;
+}
