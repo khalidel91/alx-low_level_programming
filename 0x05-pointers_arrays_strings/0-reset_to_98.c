796ff9660cac5b5eb4641f77e001b1972682a5e6
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-27 18:15:30 -0700

    first task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ *
+ * description: function that print numbers from 0 to 9
+ *
+ *
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n == 98;
+	putchar('\n');
+}
+
