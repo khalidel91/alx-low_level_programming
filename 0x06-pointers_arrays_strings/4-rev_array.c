1e19c15d245c3c27dae75beb4ba1f5c1d083984f
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-29 19:08:23 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * reverse_array - compare 2 strings
+ *
+ * @a: array to be reversed
+ * @n: size of array
+ * Return: compared strings
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int tmp;
+
+	while (i < n / 2)
+	{
+		tmp = a[i];
+		a[i] = a[(n - 1) - i];
+		a[(n - 1) - i] = tmp;
+	}
+
+}
