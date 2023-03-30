542e4c391edad5545f7b7cabccbbb64dea5ac1b2
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-03-29 18:17:57 -0700

    concatenate n function in c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strncat - prints half of a string
+ *
+ * @dest: string input
+ * @src: second string
+ * @n: n
+ * Return: concatenate
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *con = dest;
+	int i = 0;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (i < n )
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		i++;
+	}
+	*dest = '\0';
+	return (con);
+}
