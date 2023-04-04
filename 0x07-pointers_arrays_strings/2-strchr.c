9ebe06da56c2b8f0bc3e882582708b16b8f2973d
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-03 18:52:56 -0700

    search a charachter

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,13 +12,13 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (*(s + i) != '\0')
+	while (*s != '\0')
 	{
-		if (*(s + i) == c)
+		if (*s == c)
 		{
-			s += i;
 			break;
 		}
+		*s++;
 	}
 	if (*s == c)
 		return (s);
