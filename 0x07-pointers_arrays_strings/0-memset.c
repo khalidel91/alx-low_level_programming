6d2949dc598ea0eba4882e80b468e5598d99ff62
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-03 18:00:45 -0700

    first task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,20 @@
 #include "main.h"
 #include <string.h>
 /**
- * *_memset - prints half of a string
+ * _memset - prints half of a string
  *
  * @s: string input
  * @b: char
+ * @n: number
  * Return: return string
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-        unsigned int i;
+	unsigned int i;
 
-        for (i = 0; i < n; i++)
-        {
-                *(s + i) = b;
-        }
-        return (s);
+	for (i = 0; i < n; i++)
+	{
+	*(s + i) = b;
+	}
+	return (s);
 }
