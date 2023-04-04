8c6e5bff9d0a114de93a706f9367da74bfef0bf0
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-03 17:41:28 -0700

    first task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *rot13 - leet
+ *
+ * @s: string
+ *
+ * Return: leet string
+ */
+char *rot13(char *s)
+{
+	int i = 0, j;
+	char T1[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char T2[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (T1[j] == *(s + i))
+			{
+			*(s + i) = T2[j];
+			break;
+			}
+		}
+		i++;
+	}
+return (s);
+}
