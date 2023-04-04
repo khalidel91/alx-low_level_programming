0dee29c762c46b89e3bb13b586fd6dac57e2c65c
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-03 18:26:03 -0700

    search a charachter

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - locate a charachter
+ *
+ * @s: string input
+ * @c: charachter to be searched
+ * Return: pointer tp the first charachter
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *p;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			*p = *(s + i);
+			break;
+		}
+	}
+	if ((*(s + i)) == '\0')
+		return (NULL);
+	else
+		return (p);
+}
