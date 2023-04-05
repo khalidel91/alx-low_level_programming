1440c895268dd10835ef66a65f14efe7e67f125b
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-04 17:22:36 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strpbrk - strpbrk
+ *
+ * @s: string input
+ * @accept: string input
+ * Return: strpbrk function
+ */
+
+unsigned int _strpbrk(char *s, char *accept)
+	{
+	unsigned int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+				s += i;
+			}
+		}
+	if (s[0] == accept[j])
+	{
+		return (s);
+	}
+	return ('\0');
+}
