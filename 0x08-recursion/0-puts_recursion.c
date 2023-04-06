ddea1eb07be99fb6d0e32456be486a1210159cdf
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-05 18:08:49 -0700

    Recursion first task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _void _print_rev_recursion - prints half of a string
+ *
+ * @s: string input
+ * Return: return string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	while (*s != '\0')
+	{
+		_puchar(s);
+		s++;
+	}
+}
