f3083bdc104355f3032acdc37911a083cfa707fc
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-06 18:44:49 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _sqrt - power
+ *
+ * @a: int input
+ * @b: int n
+ * Return: return sqrt of given number
+ */
+
+int _sqrt(int a, int b)
+{
+	if (a * a == b)
+		return (a);
+	if (a * a > b)
+		return (-1);
+	return (a + 1, b);
+}
+
+/**
+ * _sqrt_recursion - power
+ *
+ * @n: int input
+ * Return: return sqrt of given number
+ */
+
+int _sqrt_recursion(int n)
+
+{
+
+	if (n == 0)
+	{
+	return (0);
+	}
+	return (_sqrt(1, n));
+}
