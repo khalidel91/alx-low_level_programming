ae9ce946f5d8c04d25066b202fb5f472171ae6a0
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-07 18:22:24 -0700

    ispalindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strlen - lenght of string
+ *
+ * @s: string
+ *
+ * Return: return lenght of string
+ */
+
+int _strlen(char *s)
+{
+	if (*s != '\0')
+	return (1 + _strlen(s + 1));
+}
+
+/**
+ * ispal - check if a string is pal
+ *
+ * @s : string input
+ * @i: starting charachter
+ * @j: last charachter
+ * Return: 1 if palindrime 0 else
+ */
+
+int ispal(char *s, int i, int j)
+{
+	if (*(s + i) == *(s + j))
+		if (i > j / 2)
+			return (1);
+		else
+			return (ispal(s, i + 1, j - 1));
+	else
+		return (0);
+}
+
+/**
+ * is_palindrome - prime
+ * @s: string input
+ * Return:  1 if palindrime 0 else
+ */
+
+int is_palindrome(char *s)
+
+{
+
+	return (ispal(s, 0, _strelen(s)));
+}
