d44b693d1ca2f5423a77fe9bc676adfdddd9c0d6
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-10 13:21:32 -0700

    print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
 *
 * Return: 1
 */
-int main(int __attribute__((unused)) argc, char** argv)
+int main(int __attribute__((unused)) argc, char **argv)
 
 	{
 		printf("%s\n", *argv);
-		return (1);
+		return (0);
 	}
