62791f617147ee661e86e7ff1a9a6e076f420429
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-10 13:10:49 -0700

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - our fucntion/entry point
+* @argc: argc
+* @argv: parameters
+*
+* Return: 1
+*/
+int main(int argc, *char argv)
+
+	{
+		printf("%s\n", *argv);
+		return (1);
+	}
