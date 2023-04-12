d73497fd90ad2e1f0717de737e47d7de335a0e19
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-11 19:29:38 -0700

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,6 @@ char *str_concat(char *s1, char *s2)
 	char *concat;
 	int i, len1 = 0, len2 = 0;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
 
 	while (s1[len1] != '\0')
 		len1++;
@@ -29,18 +27,6 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; i < len1; i++)
 		concat[i] = s1[i];
-	if (s1[0] == '\0')
-	{
-			concat[0] = ' ';
-			len1 = 1;
-	}
-
-	if (s2[0] == '\0')
-	{
-		concat[len1] = ' ';
-		concat[len1 + 1] = '\0';
-	}
-	else
 	for (i = len1; i < (len1 + len2); i++)
 		concat[i] = s2[i - len1];
 
