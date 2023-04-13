ba90544b64dbb2022c06f71a8803dded92b6190d
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-12 18:03:30 -0700

    free array

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* free_grid - free array
+* @grid: array to be free
+* @height: height of array
+*
+* Return: free array
+*/
+
+
+void free_grid(int **grid, int height)
+{
+	int i;
+	
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+		
+	free(grid);
+}
