7ad925e1599367aa7b3004a545ccee0a8c12a9a2
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-13 19:00:10 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _calloc - function allocate array 
+* @nmemb: input
+* @size: size input 
+*
+* Return: nconcatenated string
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	a = malloc(nmemb * size);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		a[i] = 0;
+	return (a);
+}
