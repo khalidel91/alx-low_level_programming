db401cfa2d2b1bbee896b55819f73fdc38351ac6
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-16 18:04:45 -0700

    100-argstostr

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* argstostr - concatenate function
+* @ac: input argc
+* @av: input argv
+*
+* Return:concatenates argv arguments
+*/
+
+
+char *argstostr(int ac, char **av)
+{
+{
+	char *s, *res;
+	int i, j = 0, len = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		len = len + strlen(av[i]);
+	}
+
+	s = (char *)malloc(len);
+	res = s;
+	res = (char *)malloc(len);
+
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while ((av[i][j]) != '\0')
+		{
+		*s = av[i][j];
+		j++;
+		s++;
+		}
+		*s = '\n';
+		s++;
+	}
+	return (res);
+}
