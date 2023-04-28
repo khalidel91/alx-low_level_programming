6da611ef914471fb4e7c5b4ec452cb30a48ffe31
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-27 18:49:51 -0700

    first advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * khalid_constructor - function that prints a text before main
+ *
+ * Return: void
+ */
+void khalid_constructor(void) __attribute__((constructor));
+
+void khalid_constructor(void)
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
