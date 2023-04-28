3f4fed9731a44da58b15073b3f95c1c49cd095e8
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-27 18:52:59 -0700

    first advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,6 @@ void khalid_constructor(void) __attribute__((constructor));
 
 void khalid_constructor(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
