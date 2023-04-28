3da24777d04f79f3c9f78f84c45441592840551c
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-27 17:34:15 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* print_list - first function to print lists
+* @h: pointer to the first list
+* Return: number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+size_t count = 0;
+while (h)
+{
+if (!h->str)
+printf("[%u] (nil)\n", 0);
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+count++;
+}
+return (count);
+}
