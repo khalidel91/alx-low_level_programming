134204addf964e7c4fe8525b84f082ddfad7a023
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-04-27 19:07:44 -0700

    second advanced task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,25 +1,25 @@
 section .data
-    message db 'Hello, Holberton', 0Ah ; message to be printed
-    format db '%s', 0Ah, 0 ; format string for printf
+message db 'Hello, Holberton', 0Ah ; message to be printed
+format db '%s', 0Ah, 0 ; format string for printf
 
 section .text
-    global main
+global main
 
 extern printf
 
 main:
-    ; set up stack frame
-    push rbp
-    mov rbp, rsp
+; set up stack frame
+push rbp
+mov rbp, rsp
 
-    ; call printf function to print message
-    mov rsi, message    ; second argument: message string
-    mov rdi, format     ; first argument: format string
-    xor eax, eax        ; clear eax for variadic function call
-    call printf         ; call printf function
+; call printf function to print message
+mov rsi, message    ; second argument: message string
+mov rdi, format     ; first argument: format string
+xor eax, eax        ; clear eax for variadic function call
+call printf         ; call printf function
 
-    ; clean up stack and return
-    mov rsp, rbp
-    pop rbp
-    xor eax, eax        ; return value of 0
-    ret
+; clean up stack and return
+mov rsp, rbp
+pop rbp
+xor eax, eax        ; return value of 0
+ret
