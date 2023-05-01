df72140e7451039043ca29a6db869ccfc37f5608
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-05-01 06:17:44 -0700

    firs task print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -22,10 +22,5 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 
 int _putchar(char c);
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
 
 #endif /*LISTS_H*/
