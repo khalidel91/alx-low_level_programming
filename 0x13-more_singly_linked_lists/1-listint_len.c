030c8b6068ec8e3244eebd78794b4d78b4ccf246
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-05-02 16:24:44 -0700

    size_t listint_len(const listint_t *h)

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* listint_len - first function to print lists
+* @h: pointer to the first list
+* Return: number of nodes
+*/
+size_t listint_len(const listint_t *h)
+{
+size_t count = 0;
+while (h)
+{
+if (!h->n)
+else
+h = h->next;
+count++;
+}
+return (count);
+}
