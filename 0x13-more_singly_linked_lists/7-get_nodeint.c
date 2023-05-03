e84d1998477bad2f3698743fae9d58649431b480
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-05-02 18:40:24 -0700

    get node from index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 /**
-* free_listint - first function to print lists
+* get_nodeint_at_index - first function to print lists
 * @head: pointer to the first list
+* @index: index
 * Return: number of nodes
 */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	if (head == NULL)
 		return (NULL);
-
 	while (index != 0)
 		{
 			head = head->next;
