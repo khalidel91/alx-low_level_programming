bb7870fa5f1c31adb8e9318a29e14db04250e258
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-05-02 18:43:23 -0700

    get node from index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		head = head->next;
 		if (head == NULL)
 			return (NULL);
-			index--;
-		}
+		index--;
+	}
 return (head);
 }
