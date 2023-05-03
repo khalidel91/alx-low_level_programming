2f0d1465656138bd00c3d20ddbb98e50a9e8d4b5
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-05-02 17:45:37 -0700

    free listint

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,10 +10,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *tmp;
 
-		while (head->next)
+		while (head)
 		{
 			tmp = head;
 			head = head->next;
-			free (*tmp);
+			free (tmp);
 		}
 }
