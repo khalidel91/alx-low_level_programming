458f6d122c28de545cf18419a16e1f147b8a5995
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-05-02 18:06:13 -0700

    free listint 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,10 +8,11 @@
 */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp = *head;
+	listint_t *tmp;
 
-	if (**head == NULL)
+	if (head == NULL)
 		return;
+
 	while (*head)
 	{
 		tmp = (*head)->next;
