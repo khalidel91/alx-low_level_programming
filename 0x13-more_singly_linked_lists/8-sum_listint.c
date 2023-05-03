f0b6e802bf11ab1906898c5cd6821d2a179eb8f7
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-05-02 18:46:36 -0700

    sum elements of singly list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* get_nodeint_at_index - first function to print lists
+* @head: pointer to the first list
+* @index: index
+* Return: number of nodes
+*/
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	if (head == NULL)
+		return (0);
+	while (head)
+	{
+		sum = head->n;
+		head = head->next;
+	}
+return (sum);
+}
