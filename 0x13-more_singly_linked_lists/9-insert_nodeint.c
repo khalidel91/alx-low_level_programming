581a1087b3c19ac8f0c1659ae1e7c5c1b58795f9
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-05-02 19:26:32 -0700

    sum elements of singly list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,8 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 	else
-		idx += 1;
-		while (idx != 0)
+		while (idx != 0 || tmp)
 		{
 		tmp = tmp->next;
 		if (tmp == NULL)
