50d71108e72c304db1aa6a9491c0cbbb009932bc
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-05-02 17:39:37 -0700

    free listint

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* free_listint - first function to print lists
+* @head: pointer to the first list
+* Return: number of nodes
+*/
+void free_listint(listint_t *head)
+{
+	listint_t temp;
+
+		while (head->next)
+		{
+			tmp = head;
+			head = head->next;
+			free (tmp);
+		}
+}
