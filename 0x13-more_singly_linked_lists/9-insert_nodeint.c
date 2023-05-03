0b4d959572b8d2406d1c18655fc674a627590771
Khalid Elfakir <elfakirkhalid@gmail.com>
2023-05-02 19:05:29 -0700

    sum elements of singly list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* get_nodeint_at_index - first function to print lists
+* @head: pointer to the first list
+* @index: index
+* Return: number of nodes
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *tmp = *head;
+	listint_t *new;
+
+	*new = malloc(sizeof(listint_t));
+	if (head == NULL || new == NULL)
+		return (NULL);
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	else while (idx != 0)
+	{
+		*tmp = *tmp->next;
+		if (*tmp == NULL)
+			return (NULL);
+		idx--;
+	}
+	if (new == NULL)
+		return (NULL);
+	new->next = tmp->next;
+	tmp->next = new;
+
+
+return (new);
+}
